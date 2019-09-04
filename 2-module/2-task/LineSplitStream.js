f4451a4576e6672947f12f298be74ec2472bd202
dolgovdv <dolgov.denis@gmail.com>
2019-09-04 21:06:23 +0300

    git commit -am "add solution for 2-module 2-task"

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,13 +4,39 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.lines = null;
   }
 
   _transform(chunk, encoding, callback) {
+    const count = String(chunk).indexOf(os.EOL); // проверка на os.EOL
+    const line = String(chunk).split(os.EOL);
+    if (this.lines) {
+      line[0] = this.lines + line[0];
+    }
+    if (line[line.length - 1]) { // проверка на конечный элемент
+      this.lines = line.pop(); // записываем поледний элемент в хранилище
+    }
+    console.log('lines2 = ', this.lines, 'line = ', line);
+
+    if (count === -1) { // если os.EOL нет в строке
+      console.log('lines1 = ', this.lines, 'line = ', line);
+      callback(); // данные убраны в хранилище, получаем новые данные
+    }
+    // console.log('line = ', line, ' count = ', count);
+
+    if (count !== -1) { // если os.EOL есть в строке
+      line.forEach( (item, i) => {
+        console.log('item = ', item);
+        this.push(item); // отправляем все куски между EOL
+      });
+      callback();
+    }
   }
 
-  _flush(callback) {
+  _flush(callback) { // непонятно как использовать
+    console.log('lines = ', this.lines);
+    this.push(this.lines); // надо сохранить значения из push?
+    callback();
   }
 }
-
 module.exports = LineSplitStream;
